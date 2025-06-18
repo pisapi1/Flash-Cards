f03617ed91ab837c88021719b20fb634eb4616aa
Braulio Quintana <80272979+pisapi1@users.noreply.github.com>
2025-06-18 03:35:50 -0400

    Layout Flash Card Template

---
+++ flashcards/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Quizlet Review</h1>
+      <div className='flashcard'>
+        Hi
+      </div>
+    </div>
+  )
+}
+
+export default App
